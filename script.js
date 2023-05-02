c15882c29789fea6b6d3084c564764c641bce4ce
Glitch (glitch-blank-website) <none>
2023-05-02 19:47:01 +0000

    🚣🙌 Checkpoint ./index.html:7427215/12942 ./script.js:7427215/42

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById( 'output' ).textContent += 'Async script evaluated\n';
 
+
+
 window.myAsyncFunc = function ( source ) {
   document.getElementById( 'output' ).textContent += `Async script\'s function called by ${source}\n`;
 }
